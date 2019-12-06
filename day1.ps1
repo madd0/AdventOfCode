faaf27c9ea50302fe799e7e3ddf14971d49a02de
Mauricio Díaz Orlich <github@madd0.com>
2019-12-06 18:47:02 +0100

    Day 1 - Part 2

--- day1.ps1
+++ day1.ps1
@@ -1,6 +1,21 @@
-Get-Content day1.txt | ForEach-Object {$total = 0} { 
-    $mass = [int]::Parse($_)
+function Get-Fuel([int]$mass) {
     $fuel = [System.Math]::Floor($mass / [int]3) - 2
-    $total += $fuel } {$total}
+    
+    if ($fuel -lt 0) {
+        return 0
+    }
+    
+    $fuel + (Get-Fuel $fuel)
+}
+
+
+$data = Get-Content day1.txt
+
+$data | ForEach-Object {$total = 0} { 
+    $mass = [int]::Parse($_)
+    $fuel = Get-Fuel $mass
+    $total += $fuel
+} {$total}
 
 # Part 1: 3427947
+# Part 2: 5139037
